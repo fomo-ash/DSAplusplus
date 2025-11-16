523497287926bdf4f3d824c3d07d11b7018e33fb
fomo-ash <ashutoshbadapanda02@gmail.com>
2025-11-16 16:43:06 +0530

    voting algo

---
+++ array/longsubwithsumk.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void left(int arr[], int n) {
+   
+    int last=arr[n-1];
+    
+ for(int i=n-2; i>=0; i--){
+    arr[i+1]=arr[i];
+ }
+
+  arr[0]=last;
+
+    
+}
+
+int main() {
+    int arr[6] = {2,3,4,5,2,6};
+ 
+    left(arr, 6);
+    
+    for(int i=0; i<6; i++){
+        cout<<arr[i];
+    }
+}
+  
